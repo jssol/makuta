209ac5245a5eeb28ab857408fc63a6d457592b30
Jonathan Sivahera <jsivahera@icloud.com>
2024-06-17 04:12:36 +0200

    Add Bank accounts overview component

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp
+        end={amount}
+        decimal=","
+        decimals={2}
+        prefix="$"
+      />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
