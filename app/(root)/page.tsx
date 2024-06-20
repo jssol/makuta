be24cf88aa0716c07846794e40117d9d7194b286
Jonathan Sivahera <jsivahera@icloud.com>
2024-06-20 23:57:22 +0200

    Build template for Right Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from '../../components/HeaderBox';
 import TotalBalanceBox from '../../components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
-  const loggedIn = { firstName: "Jonathan" };
+  const loggedIn = { firstName: "Jonathan", lastName: "Sivahera", email: "contact@" };
 
   return (
     <section className="home">
@@ -22,7 +23,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   );
 };
