c5a184872d9de52ec8132c623420f626e56c4d70
Jonathan Sivahera <jsivahera@icloud.com>
2024-06-28 23:34:19 +0200

    User Next.js import syntax

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import HeaderBox from '../../components/HeaderBox';
-import TotalBalanceBox from '../../components/TotalBalanceBox';
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
